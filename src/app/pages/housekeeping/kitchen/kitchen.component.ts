6061aaf8ed1e1fc7bf52fabdf75b1189887075ea
CristinaPas <cri.paschina@hotmail.it>
2026-03-01 10:04:25 +0100

    Separate kitchen in two containers and add color background

--- src/app/pages/housekeeping/kitchen/kitchen.component.ts
+++ src/app/pages/housekeeping/kitchen/kitchen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-kitchen',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './kitchen.component.html',
   styleUrl: './kitchen.component.css'
 })
