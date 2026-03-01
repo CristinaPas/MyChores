abd5d14bd5020c7eb58285146a8e3eb7ead2e572
CristinaPas <cri.paschina@hotmail.it>
2026-03-01 13:48:34 +0100

    Kitchen component, add service connection

--- src/app/pages/housekeeping/kitchen/kitchen.component.ts
+++ src/app/pages/housekeeping/kitchen/kitchen.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'app-kitchen',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './kitchen.component.html',
   styleUrl: './kitchen.component.css'
 })
 export class KitchenComponent {
+
+  newTaskTitle = ''; //create and instantiate an empty string to hold the new task title
+  constructor(public taskService: TaskService) {} //we inject here the TaskService to use its methods and properties in our component
+  showInputField = signal(false);
+
+  addTask() {
+    if (!this.newTaskTitle.trim()) return;
+
+    this.taskService.addTask(this.newTaskTitle);
+    this.newTaskTitle = '';
+    this.showInputField.set(false);
+    //every time we add a task, we hide the input field again by setting the signal to false again 
+  }
+
+  openInput() {
+    this.showInputField.set(true); 
+  }
 
 }
